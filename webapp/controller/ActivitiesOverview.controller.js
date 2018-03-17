2da375f01646c899a57fdaec6a392f70e2319df4
hterminasyan <hterminasyan@gmail.com>
2018-03-17 09:15:47 +0000

    ActivityOverview page

--- webapp/controller/ActivitiesOverview.controller.js
+++ webapp/controller/ActivitiesOverview.controller.js
@@ -1,18 +1,61 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"com/sap/healtybiker/HealtyBiker/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/routing/History",
+	"com/sap/healtybiker/HealtyBiker/model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/Sorter"
+], function(BaseController, JSONModel, History, formatter, Filter, FilterOperator, Sorter) {
 	"use strict";
 
-	return Controller.extend("com.sap.healtybiker.HealtyBiker.controller.ActivitiesOverview", {
+	return BaseController.extend("com.sap.healtybiker.HealtyBiker.controller.ActivitiesOverview", {
+
+		formatter: formatter,
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf com-gameofcode-bikerappbikerapp.view.ActivitiesOverview
 		 */
-		//	onInit: function() {
-		//
-		//	},
+		onInit: function() {
+
+		},
+
+		onBeforeRendering: function() {
+			var oModel = this.getView().getModel();
+			var aFilter = [new Filter("C_TRIPID", FilterOperator.EQ, 1)];
+			oModel.read("/ACME.T_IOT_D225176AE8FB5D12880E", {
+				filters: aFilter,
+				success: function(oData) {
+					var oReviewCycleModel = new sap.ui.model.json.JSONModel(oData.results);
+					this.getView().setModel(oReviewCycleModel, "iotDataModel");
+				}.bind(this),
+				error: function(oError) {
+					var sResponseText = JSON.parse(oError.responseText);
+					jQuery.sap.log.error(sResponseText);
+				}.bind(this)
+			});
+		},
+
+		onTripChanged: function(oEvent) {
+
+			var oModel = this.getView().getModel();
+			var sSelection = oEvent.getSource().getSelectedKey();
+			var aFilter = [new Filter("C_TRIPID", FilterOperator.EQ, sSelection)];
+			oModel.read("/ACME.T_IOT_D225176AE8FB5D12880E", {
+				filters: aFilter,
+				success: function(oData) {
+					var oReviewCycleModel = new sap.ui.model.json.JSONModel(oData.results);
+					this.getView().setModel(oReviewCycleModel, "iotDataModel");
+				}.bind(this),
+				error: function(oError) {
+					var sResponseText = JSON.parse(oError.responseText);
+					jQuery.sap.log.error(sResponseText);
+				}.bind(this)
+			});
+
+		},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
@@ -39,6 +82,31 @@ sap.ui.define([
 		//	onExit: function() {
 		//
 		//	}
+
+		onNavigateActivities: function(oEvent) {
+			this.getRouter().navTo("activities");
+		},
+		onNavigateProfile: function(oEvent) {
+			this.getRouter().navTo("profile");
+		},
+		onNavigateTripOverview: function(oEvent) {
+			this.getRouter().navTo("tripoverview");
+		},
+		onNavigateWelcome: function(oEvent) {
+			this.getRouter().navTo("welcomescreen");
+		},
+
+		onNavBack: function() {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.navTo("overview", true);
+			}
+		}
 
 	});
 
